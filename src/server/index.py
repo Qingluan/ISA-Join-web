094eafd20fff0be238effc913992f9f11c7bf791
ppph <857814091@qq.com>
2014-10-09 03:35:17 +0800

    add index

--- src/server/index.py
+++ src/server/index.py
@@ -1,14 +1,14 @@
 import tornado.ioloop
 import tornado.web
 import motor
+from handlers import IndexHandler
 from handlers import PushMessageHandler
 from handlers import LoginHandler
 from handlers import SignUpHandler
 from env import settings
 
-
-
 application = tornado.web.Application([
+	(r"/",IndexHandler),
 	(r"/SignUp",SignUpHandler),
 	(r"/PushMessage",PushMessageHandler),
 	(r"/Login",LoginHandler),
